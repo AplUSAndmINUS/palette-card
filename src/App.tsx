2d13fbb2edd8359047191d06302bae016c5d31ae
Terence Waters <terence.waters@imail.org>
2020-03-28 18:52:45 -0600

    updated form entry to accept hex value

--- src/App.tsx
+++ src/App.tsx
@@ -5,43 +5,79 @@ import Card from './components/Card';
 import Header from './components/Header';
 
 const App = () => {
-  const [color, setColor] = React.useState('#3844C3');
-  
+  const [color, setColor] = React.useState('');
+  const [dispColor, setDispColor] = React.useState('#BCBCBC');
+  const [validated, setValidate] = React.useState(false);
+
+  const handleChange = ({ target }: any) => {
+    setColor(target.value);
+  };
+
+  const handleClear = () => {
+    setColor('');
+    setDispColor('#BCBCBC');
+  };
+
+  const handleSubmit = () => {
+    const regexp = /^[0-9A-F]+$/;
+    if (color.length === 6) {
+      if (regexp.test(color)) {
+        setDispColor(`#${color.toString()}`);
+        setValidate(true);
+      }
+    } else if (color.length === 7) {
+      if (color.charAt(0) === '#') {
+        if (regexp.test(color.substr(1, 7))) {
+          setDispColor(color);
+          setValidate(true);
+        }
+      }
+    }
+  };
+
   return (
     <>
-    <Header />
-    <Container>
-      <Row>
-        <Col sm={8}>
-          <Form>
-            <Form.Group>
-              <Form.Label>Enter a color hex value</Form.Label>
-              <Form.Control
-                type="text"
-                name="colorTxt"
-                placeholder="#000000"
-                value={color}
-              ></Form.Control>
-            </Form.Group>
-          </Form>
-        </Col>
-      </Row>
-      <Row>
-        <Col sm={8}>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>{' '}
-          <Button variant="outline-secondary" type="reset">
-            Clear
-          </Button>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Card color={color} />
-        </Col>
-      </Row>
-    </Container>
+      <Header />
+      <Container>
+        <Row>
+          <Col sm={8}>
+            <Form noValidate validated={validated} onSubmit={handleSubmit}>
+              <Form.Group>
+                <Form.Label>Enter a color hex value</Form.Label>
+                <Form.Control
+                  defaultValue=""
+                  id="colorTxt"
+                  type="text"
+                  maxLength={7}
+                  name="colorTxt"
+                  onChange={handleChange}
+                  placeholder="000000"
+                  value={color}
+                ></Form.Control>
+              </Form.Group>
+            </Form>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={8}>
+            <Button variant="primary" type="submit" onClick={handleSubmit}>
+              Submit
+            </Button>{' '}
+            <Button
+              variant="outline-secondary"
+              type="reset"
+              onClick={handleClear}
+            >
+              Clear
+            </Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Card color={dispColor} />
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
